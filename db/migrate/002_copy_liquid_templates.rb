0e97992035292feb4844135e6b7f66f4c0527efd
Simon King <simon@organicinternet.co.uk>
2009-12-30 23:09:44 +0000

    Added 'development' to theme path

--- db/migrate/002_copy_liquid_templates.rb
+++ db/migrate/002_copy_liquid_templates.rb
@@ -1,7 +1,7 @@
 class CopyLiquidTemplates < ActiveRecord::Migration
   def self.up
-    FileUtils.cp(File.join(RAILS_ROOT, 'vendor', 'plugins', 'mephisto_gallery', 'templates', 'album.liquid'), File.join(RAILS_ROOT, 'themes', 'site-1', 'simpla', 'templates'))
-    FileUtils.cp(File.join(RAILS_ROOT, 'vendor', 'plugins', 'mephisto_gallery', 'templates', 'gallery.liquid'), File.join(RAILS_ROOT, 'themes', 'site-1', 'simpla', 'templates'))
+    FileUtils.cp(File.join(RAILS_ROOT, 'vendor', 'plugins', 'mephisto_gallery', 'templates', 'album.liquid'), File.join(RAILS_ROOT, 'themes', 'development', 'site-1', 'simpla', 'templates'))
+    FileUtils.cp(File.join(RAILS_ROOT, 'vendor', 'plugins', 'mephisto_gallery', 'templates', 'gallery.liquid'), File.join(RAILS_ROOT, 'themes', 'development', 'site-1', 'simpla', 'templates'))
   end
   
   def self.down
